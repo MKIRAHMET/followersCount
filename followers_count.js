d6091a87fab665a7195b81e7258eefa19625c469
Muhammed <muhammed@192.168.2.16>
2025-01-30 22:12:38 +0200

    followers Count

---
+++ followers_count.js
@@ -0,0 +1,22 @@
+let count = 0;
+
+
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+function checkCountValue(){
+    if (count == 10) {
+        alert("Your instagram post gained 10 followers! Congratulations!");
+    }else if (count == 20){
+        alert("Your instagram post gained 20 followers! You're almost there!");
+    }
+}
+
+function increaseCount() {
+    count++;
+    console.log(`Count: ${count}`);
+    displayCount();
+    checkCountValue();
+}
